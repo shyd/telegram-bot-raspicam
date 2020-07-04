e323228dab0a8d37707f08332131581436451ab0
Dennis Schütt <dennis@shyd.de>
2020-07-04 16:55:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,13 +17,11 @@ const keyboard = Markup.inlineKeyboard([
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Hello'))
-bot.help((ctx) => ctx.reply('Help message'))
 
 bot.command('pic', (ctx) => {
     camera.takePhoto().then((photo) => {
         ctx.replyWithPhoto({ source: photo })
     });
 })
-bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.chat.id, ctx.message, Extra.markup(keyboard)))
 bot.action('delete', ({ deleteMessage }) => deleteMessage())
 bot.launch()

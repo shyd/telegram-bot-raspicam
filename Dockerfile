2af567222c6bb5745da5ba0301d5eec77178ecc0
Dennis Schütt <dennis@shyd.de>
2020-07-05 14:15:26 +0200

    add video

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ RUN echo "/opt/vc/lib" > /etc/ld.so.conf.d/00-vcms.conf
 WORKDIR /usr/src/app
 
 RUN mkdir photos
+RUN mkdir videos
 
 COPY . .
 RUN npm install
--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const Markup = require('telegraf/markup')
 const Raspistill = require('node-raspistill').Raspistill;
 const camera = new Raspistill({
     verticalFlip: true,
+    horizontalFlip: true
+});
+const Raspivid = require('node-raspivideo');
+const raspivid = new Raspivid({
+    videoFolder: './videos',
+    verticalFlip: true,
     horizontalFlip: true,
-    width: 1920,
-    height: 1080
+    format: 'mp4'
 });
 
 const keyboard = Markup.inlineKeyboard([
@@ -22,6 +27,18 @@ bot.command('pic', (ctx) => {
     camera.takePhoto().then((photo) => {
         ctx.replyWithPhoto({ source: photo })
     });
+})
+bot.command('vid', (ctx) => {
+    const now = Math.floor(Date.now() / 1000);
+    raspivid.record(now, 10000)
+        .then(() => {
+            console.log('record done!');
+            ctx.replyWithVideo({ source: './videos/'+now+'.mp4' })
+        })
+        .catch((err) => {
+            console.error(err);
+            ctx.reply('sorry, could not record video!')
+        });
 })
 bot.action('delete', ({ deleteMessage }) => deleteMessage())
 bot.launch()
